465006eba2bc3b96ea881debc2dea4bceedc1e37
Wayne <djs860403@gmail.com>
2021-10-16 23:05:08 +0800

    Create hover get imgSrc
    1. Toggle "GET_CONTENT_SHOOT_FROM_SITE" to get imgSrc.
    2. Register "IMGSRC_FROM_EXTENSION" response to get imgSrc.

--- src/components/ListShoots.js
+++ src/components/ListShoots.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
 import styles from "./ListShoots.module.css"
+import { PostToContent, registerCB } from "../util/Comm"
 
 const DEFAULT_IMG = "https://i.pinimg.com/564x/51/45/c0/5145c0dc420e967fc4c0f8f72e20c3a2.jpg"
 
 export default class ListShoots extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            previewImgSrc: [],
+        }
+    }
+    // handler
+    itemHover = (e = null, tabId = "", getItem = "") => {
+        if (e == null)
+            return
+        const target = e.currentTarget
+        console.log(target, tabId, getItem)
+
+        // toggle content to get imgSrc
+        PostToContent({
+            type: "GET_CONTENT_SHOOT_FROM_SITE",
+            tabId: tabId,
+            getItem: getItem
+        })
     }
 
+    getImgSrc = (data) => {
+        console.log(data)
+    }
     // conponents
     eachItemComponent = (contentList = null) => {
         if (contentList == null)
@@ -17,14 +37,15 @@ export default class ListShoots extends Component {
             .filter(item => Object.keys(item).length != 0 && item.res.length > 0)
             .map((item, i) => {
                 console.log(item)
+                const tabId = item.id
                 const contents = item.res.map((text, i) => {
-                    return <div key={i} className={styles.contentSrcWrapper}>{text}</div>
+                    return <div key={i} className={styles.contentSrcWrapper} onMouseEnter={(e) => { this.itemHover(e, tabId, text) }}>{text}</div>
                 })
                 return (
                     <div key={i} className={styles.ItemWrapper}>
                         {item.title}
                         <div className={styles.ItemImgListWrapper}>
-                            <div className={styles.ItemTextWrapper}>
+                            <div className={styles.ItemTextWrapper} >
                                 {contents}
                             </div>
                             <div className={styles.ItemThumbnailWrapper}>
@@ -37,6 +58,9 @@ export default class ListShoots extends Component {
         return listComponent
     }
 
+    componentDidMount() {
+        registerCB({ type: "IMGSRC_FROM_EXTENSION", callback: this.getImgSrc })
+    }
     render() {
 
         return (
