978259ad51a9d087967ca55d7a7a6baadca9c0f3
Wayne <djs860403@gmail.com>
2021-10-17 22:12:18 +0800

    Append to img list
    1. Using componentDidUpdate to check img been updated.
    2. After updated, set to append into plateImgList to show up.

--- src/components/ContentPlate.js
+++ src/components/ContentPlate.js
@@ -5,13 +5,42 @@ export default class ContentPlate extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            plateImgList: [],
+        }
+    }
 
+    // component
+    plateComponent = () => {
+        const plateItem = this.state.plateImgList.map((item, i) => {
+            return (
+                <div key={i} className={styles.itemWrapper}>
+                    <img src={item} alt="" />
+                </div>
+            )
+        })
+        return (
+            <div className={styles.plateWrapper}>
+                {plateItem}
+            </div>
+        )
+    }
+    // life cycle
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.pickImg != this.props.pickImg) {
+            this.setState(state => {
+                return {
+                    plateImgList: [...state.plateImgList, this.props.pickImg]
+                }
+            })
         }
     }
     render() {
         return (
             <div className={styles.container}>
                 <img src={this.props.pickImg} alt="" srcset="" />
+                <div className={styles.plateContainerStyle}>
+                    {this.plateComponent()}
+                </div>
             </div>
         )
     }
