91f32d9a22d7d9ec9ea1abb48d1df11ba849f7a6
Wayne <djs860403@gmail.com>
2021-12-05 23:43:19 +0800

    Layout HomeHeader
    1. Set up descprtion and video manual.
    2. Layout left and right side for the first sight.

--- src/components/HomeComponents/HomeHeader.js
+++ src/components/HomeComponents/HomeHeader.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react'
+import styles from "./HomeHeader.module.css"
+// modules
+import YouTube from 'react-youtube';
 
 export default class HomeHeader extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    // Handler
+
+    // sub-components
+    DespProductComponent = () => {
+        return (<div className={styles.despStyle}>
+            <div className={styles.despTitleStyle}>
+                <h1>Oolet</h1>
+                <p>
+                    Screenshot visible web page and snap to anywhere in the site
+                    after website loading finished, you can shot mulitple images and save meaningful part
+                    also show in the site
+                </p>
+                <p>
+                    Oolet extension is created for user want to shot several times in one site
+                    Also, want to see the different paragraph at the same time
+                </p>
+            </div>
+            <div className={styles.despDetailStyle}></div>
+        </div>)
+    }
+
+    VideoDespComponent = () => {
+        const opts = {
+            height: '720',
+            width: '1280',
+            playerVars: {
+                // https://developers.google.com/youtube/player_parameters
+                autoplay: 1,
+            },
+        };
+        return (
+            <div className={styles.vidManualStyle}>
+                <YouTube videoId="R1ovtJ70sug" opts={opts} />
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div>
-                HomeHeader
+            <div className={styles.container}>
+                <this.DespProductComponent />
+                <this.VideoDespComponent />
             </div>
         )
     }
