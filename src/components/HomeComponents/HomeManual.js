0c6106f10079b34d62087d0621fc50e3e73526b5
Wayne <djs860403@gmail.com>
2021-12-06 23:07:52 +0800

    Layout Manual component
    1. Add manualCard to show manual content by image and pop description.
    2. Set visible for popDespStyle by triggering paranet hover event.

--- src/components/HomeComponents/HomeManual.js
+++ src/components/HomeComponents/HomeManual.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react'
+import styles from "./HomeManual.module.css"
+
+const IMG_SRC = "https://i.pinimg.com/564x/5b/8b/a6/5b8ba662dafbe11a4a67137628818f04.jpg"
 
 export default class HomeManual extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            list: [
+                { imgSrc: IMG_SRC, title: "Stitch", popDesp: "Lorem" },
+                { imgSrc: IMG_SRC, title: "Stitch", popDesp: "Pop click" },
+                { imgSrc: IMG_SRC, title: "Stitch", popDesp: "Show up" },
+                { imgSrc: IMG_SRC, title: "Stitch", popDesp: "Show up" },
+            ]
+        }
+    }
+
+    // handler
+
+    // sub-component
+    manualCard = ({ key = 0, imgSrc = IMG_SRC, title = "", popDesp = "" }) => {
+        return (
+            <div key={key} className={styles.manualCardStyle}>
+                <div className={styles.popDespStyle}>{popDesp}</div>
+                <div className={styles.manualCardImgWrapper}>
+                    <img src={imgSrc} alt="" />
+                </div>
+                <h2>{title}</h2>
+            </div>
+        )
+    }
+
+    manualCardWrapper = () => {
+        const cardGroup = this.state.list.map((item, i) => {
+            return this.manualCard({ key: i, ...item })
+        })
+        return (
+            <div className={styles.cardWrapperStyle}>
+                {cardGroup}
+            </div>
+        )
+    }
     render() {
         return (
-            <div>
-                HomeManual
+            <div className={styles.container}>
+                <h1>HomeManual</h1>
+                <this.manualCardWrapper />
             </div>
         )
     }
