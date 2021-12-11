7f863ec0229a304c07f314dc60febb4f1c214d4c
Wayne <djs860403@gmail.com>
2021-12-11 12:42:31 +0800

    Create DownloadPage
    1. Style download as compare list to show each plan property.
    2. Using flex to list each one and set up width for each block to make
       alignment.

--- src/components/DownloadProPage.js
+++ src/components/DownloadProPage.js
@@ -6,6 +6,20 @@ import FireBaseFunc from "../util/FirebaseFunc"
 export default class DownloadProPage extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            compareList: [
+                ["", ["Free", "Premium"]],
+                ["Moving image", ["V", "V"]],
+                ["Scale", ["V", "V"]],
+                ["Crop", ["V", "V"]],
+                ["Adjust Opacity", ["V", "V"]],
+                ["Hidden", ["V", "V"]],
+                ["Get shoot from other tabs", ["V", "V"]],
+                ["Download", ["V", "V"]],
+                ["Search", ["V", "V"]],
+                ["Multi-Shoot", ["3 limitation", "V"]],
+            ]
+        }
     }
 
 
@@ -37,21 +51,41 @@ export default class DownloadProPage extends Component {
         )
     }
 
+    proportyFunc = (key = 0, property = "", list = []) => {
+        const checkList = list.map((item, key) => {
+            return (
+                <div key={key} className={styles.propertyCheckStyle}>{item}</div>
+            )
+        })
+        return (
+            <div key={key} className={styles.propertyWrapper}>
+                <div className={styles.propertyTitleStyle}>{property}</div>
+                {checkList}
+            </div>
+        )
+    }
+
+    checkButton = (title) => {
+        return (
+            <div className={styles.checkWrapperStyle} onClick={this.downloadHandler}>
+                {title}
+            </div>
+        )
+    }
+
     render() {
 
-
+        const compareListComponent = this.state.compareList.map((items, key) => {
+            return this.proportyFunc(key, items[0], items[1])
+        })
+        const len = this.state.compareList.length
 
         return (
             <div className={styles.container}>
-
-                <div className={styles.downloadLayoutStyle}>
-                    <this.cardComponent title="Free extension" clickHandler={this.downloadHandler} />
-                    <this.cardComponent title="Premium extension" clickHandler={this.downloadHandler} />
-                </div>
-
                 <div className={styles.compareListWrapper}>
+                    {compareListComponent}
+                    {this.proportyFunc(len + 1, "", [this.checkButton("Download"), this.checkButton("Buy now")])}
                 </div>
-
             </div>
         )
     }
