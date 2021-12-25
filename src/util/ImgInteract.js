d4eabb7c610a44800f373a83b2c4f847328b3fec
Wayne <djs860403@gmail.com>
2021-12-25 16:33:54 +0800

    Rearrange img and wrapper
    1. Setting wrapper to wrap img src, to hidden oversize area.
    2. Add crop id for crop handler.
    3. Style in css to set position and color.

--- src/util/ImgInteract.js
+++ src/util/ImgInteract.js
@@ -7,6 +7,11 @@ const LTID = "scaleLT"
 const RTID = ""
 const LBID = ""
 
+const CROP_RBID = "cropRB"
+const CROP_LTID = "cropLT"
+const CROP_RTID = ""
+const CROP_LBID = ""
+
 const SCALE_OFFSET = 10
 // with scale, crop and rotation
 export default class ImgInteract extends Component {
@@ -16,8 +21,8 @@ export default class ImgInteract extends Component {
 
         this.state = {
             scale: {
-                width: "250px",
-                height: "250px",
+                width: "",
+                height: "",
             },
             crop: {
 
@@ -27,12 +32,22 @@ export default class ImgInteract extends Component {
             zIndex: 0
         }
 
+        // TODO: merge as one event queue
         this.scaleFlag = false;
-        this.scaleDir = null
-        this.node = null
-        this.nodeStaticRect = {}
-
         this.moveFlag = false;
+        this.cropFlag = false;
+
+        //three instance
+        this.node = null
+        this.nodeImgWrapper = null
+        this.nodeImg = null
+
+        this.nodeStaticRect = {}
+        this.imgRect = {}
+
+        this.scaleDir = null
+        this.cropDir = null
+
     }
 
     // 
@@ -43,6 +58,17 @@ export default class ImgInteract extends Component {
         switch (target.id) {
             case this.props.id:
                 this.moveFlag = !this.moveFlag;
+                break;
+            case CROP_RBID:
+            case CROP_LTID:
+            case CROP_RTID:
+            case CROP_LBID:
+                this.cropFlag = !this.cropFlag
+                if (this.cropFlag == true) {
+                    this.cropDir = e.currentTarget.getAttribute("id")
+                    this.imgRect = this.nodeImg.getBoundingClientRect()
+                    // console.log(this.node.childNodes[0].childNodes[0].getBoundingClientRect())
+                }
                 break;
             case RBID:
             case LTID:
@@ -72,11 +98,12 @@ export default class ImgInteract extends Component {
     scaleHandler = (e) => {
         if (this.scaleFlag) {
             var { top, left, right, bottom, width, height } = this.node.getBoundingClientRect()
+
             var mX = e.clientX
             var mY = e.clientY
             var w = width;
             var h = height;
-            console.log(this.scaleDir, mY, mX, this.node.offsetLeft, this.node.style.transform)
+            // console.log(this.scaleDir, mY, mX, this.node.offsetLeft, this.node.style.transform)
 
             //RB estimate
             if (this.scaleDir == RBID) {
@@ -98,6 +125,46 @@ export default class ImgInteract extends Component {
             }
         }
     }
+
+    cropHandler = (e) => {
+        if (this.cropFlag) {
+            var { top, left, right, bottom, width, height } = this.node.getBoundingClientRect()
+            var { top: imgTop, left: imgLeft, width: imgWidth, height: imgHeight } = this.imgRect
+
+            console.log(imgTop, imgLeft, imgWidth, imgHeight)
+
+
+            var mX = e.clientX
+            var mY = e.clientY
+            var w = width;
+            var h = height;
+
+            //RB estimate
+            if (this.cropDir == CROP_RBID) {
+                w = mX - left
+                h = mY - top
+                this.node.style.width = `${w}px`
+                this.node.style.height = `${h}px`
+
+            }
+
+            //LT estimate
+            if (this.cropDir == CROP_LTID) {
+                w = right - mX
+                h = bottom - mY
+
+                this.node.style.width = `${w}px`
+                this.node.style.height = `${h}px`
+                this.node.style.top = `${mY - (this.nodeStaticRect.top)}px`
+                this.node.style.left = `${mX - (this.nodeStaticRect.left)}px`
+            }
+
+            // this.nodeImg.style.width = `${imgWidth}px`
+            // this.nodeImg.style.height = `${imgHeight}px`
+            // this.nodeImg.style.top = `${imgTop - mY}px`
+            // this.nodeImg.style.left = `${imgLeft - mX}px`
+        }
+    }
     // life-cycle handler
     componentDidMount() {
         // console.log(this.node)
@@ -105,30 +172,37 @@ export default class ImgInteract extends Component {
             //select which func should be invoked
             this.scaleHandler(e)
             this.moveHandler(e)
+            this.cropHandler(e)
         })
     }
 
-    render() {
+    refMount = (ref) => {
+        this.node = ref
 
-        const wrapperStyle = {
-            position: "relative",
-            background: "grey",
-            width: this.state.scale.width,
-            height: this.state.scale.height,
-            top: 0,
-            left: 0
+        if (this.node != null) {
+            this.nodeImgWrapper = this.node.childNodes[0]
+            this.nodeImg = this.node.childNodes[0].childNodes[0];
+            this.nodeStaticRect = this.node.getBoundingClientRect()
         }
+    }
 
+    render() {
         return (
-            <div style={wrapperStyle} ref={(node => {
-                this.node = node
-                this.nodeStaticRect = this.node.getBoundingClientRect()
-            })} >
-                <img id={this.props.id} className={styles.imgStyle} src={this.props.src} alt="" srcset="" onClick={(e) => this.toggleScaleHandler(e)} />
+            <div className={styles.container} ref={this.refMount} >
+                <div className={styles.imgWrapper}>
+                    <img id={this.props.id} className={styles.imgStyle} src={this.props.src} alt="" srcset="" onClick={(e) => this.toggleScaleHandler(e)} />
+                </div>
+
                 <div id={RBID} className={styles.scaleRBStyle}
                     onMouseUp={(e) => this.toggleScaleHandler(e)}
                 ></div>
                 <div id={LTID} className={styles.scaleLTStyle}
+                    onMouseUp={(e) => this.toggleScaleHandler(e)}
+                ></div>
+                <div id={CROP_RBID} className={styles.cropRBStyle}
+                    onMouseUp={(e) => this.toggleScaleHandler(e)}
+                ></div>
+                <div id={CROP_LTID} className={styles.cropLTStyle}
                     onMouseUp={(e) => this.toggleScaleHandler(e)}
                 ></div>
             </div>
